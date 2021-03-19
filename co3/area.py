e776412544c06b5dcc943eef621ed0c70fca2b56
nourin <65014502+fathimanovrin@users.noreply.github.com>
2021-03-19 12:59:35 +0530

    main

---
+++ co3/area.py
@@ -0,0 +1,36 @@
+from graphics.rectangle import *
+from graphics.circle import *
+from graphics.DGraphics.cuboid import *
+from graphics.DGraphics.sphere import *
+print ("rectangle")
+l=int(input("enter length"))
+b=int(input("enter breadth"))
+print("are of rectangle",RectArea(1,b))
+print("perimeter of rectangle",Rectperimeter(1,b))
+print()
+print ("circle")
+r=int(input("enter radius"))
+print("area of circle",circlearea(r))
+print("perimeter of circle",circleperimeter(r))
+print()
+print ("cuboid")
+l=int(input("enter length"))
+w=int(input("enter width"))
+h=int(input("enter height"))
+print("are of cuboid",cuboidarea(1,w,h))
+print("perimeter of cuboid",cuboidperimeter(1,w,h))
+print()
+print ("shpere")
+r=int(input("enter radius"))
+print("area of shpere",spherearea(r))
+print("perimeter of shpere",sphereperimeter(r))
+print()
+
+
+
+
+
+
+
+            
+            
